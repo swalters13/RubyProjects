af9f2bbfacdd29f8490d12152c24bd7cbea759c1
swalters13 <swalters13@gmail.com>
2016-12-07 22:51:10 -0800

    Fixed cost issue
    Fixed issue of not calculating total cost per customer.

--- Puppet Coding Exercises/CustomerInfo1.rb
+++ Puppet Coding Exercises/CustomerInfo1.rb
@@ -21,22 +21,26 @@ inFile = CSV.read("customerinfo.csv", "r")
 # Sort the CSV file by customer name while leaving the header line alone
 sorted = [inFile.first] + inFile[1..-1].sort_by{|x| x[2]}
 
-# Determine how many orders each customer has placed
-custOrderCount = Hash.new(0)
-custOrderPrice = Hash.new(0)
-sorted[1..-1].each do |order,id,name,sku,price|
-  custOrderCount[name] += 1
+for orders in sorted[1..-1]
+  puts "#{orders}"
 end
 
-# Determine how much each customer has spend on average across all their purchases - !!This is Psudocode!!
-for each customer in sorted[1..-1]
-  totalCustomerPrice += sorted[4]
-  aveCustomerPrice = totalCustomerPrice / custOrderCount['customername']
 
-#Coalate the data - !!This is Psudocode!!
-while i < sorted.length
-  custData[i] = [ "customername",custOrderCount['customername'], aveCustomerPrice]
+# Determine how many orders each customer has placed and the total they have spent
+custOrderCount = Hash.new(0)
+custTotalPrice = Hash.new(0)
+
+sorted[1..-1].each do |order,id,name,sku,price|
+  custOrderCount[name] += 1
+  custTotalPrice[name] += price.delete('$').to_f
+end
+
+puts "#{custOrderCount}"
+puts "#{custTotalPrice}"
+
+#Coalate the data
+
  
 
 # Send the results to output in XML
-customer_xml(custData)
+#customer_xml(custData)
