bc28eb11cd930ac8fdcc7708654cb4681392f67a
swalters <swalters13@gmail.com>
2016-12-14 09:35:31 -0800

    Recent updates

--- LargestPrimeFactor.rb
+++ LargestPrimeFactor.rb
@@ -7,22 +7,32 @@ num1 = 600851475143
 primefactor = 0
 x = 1
 
-num2 = Math.sqrt(num1).round + 1
+num2 = (num1 / 2).round.to_i
+num3 = Math.sqrt(num1).round+1
 
-while x > 0
-  puts "Num2 = #{num2}"
-  if num1 % num2 == 0
-    if num2 % 2 != 0
-      primefactor = num2
-      x = 0
-    end
-  else
-    num2 -= 1
-    if num2 % 2 == 0
-      num2 -= 1
-    end    
+def isPrime ( int1 )
+	puts "Checking if #{int1} is not divisible by 2 and greater than 2."
+	if int1 % 2 != 0 && int1 > 2
+		puts "Checking if #{int1} is prime."
+		(3..(Math.sqrt(int1).round+1)).step(2).all? {|x| int1 % x == 0}
+	else
+		return false
+	end
+end
+
+while x < num2
+  #puts "x = #{x}"
+  if num1 % x == 0 && x > 2
+  	puts "#{num1} is divisible by #{x}"
+  	if isPrime(x)
+   	puts "The prime factor is #{x}"
+    primefactor = x
+   end  
   end
-  puts "x = #{x}"
+  x+=1
+  if x % 2 == 0
+  	x += 1
+  end
 end
 
 puts "The largest prime factor is #{primefactor}"
--- Puppet Coding Exercises/PrintIfPrime.rb
+++ Puppet Coding Exercises/PrintIfPrime.rb
@@ -3,6 +3,22 @@ def isPrime( num1 )
   if num1 % 2 == 0 && num2 > 2
     return False
   else
-    
+    (3..Math.sqrt(num1).round + 1).step(2) do |x|
+    		if num1 % x ==0
+    			return True
+    		else
+    			return False
+    		end
+    end
   end
+end
+
+
+
+int1 = Array.new (1..1000)
+
+for n in int1
+	if isPrime(n)
+		puts "The sum of the digits of the number #{n} is prime."
+	end
 end
