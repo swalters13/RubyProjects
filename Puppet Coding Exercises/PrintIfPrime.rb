25e58da8855c90b2a4414c94c2130a53af818e61
swalters13 <swalters13@gmail.com>
2016-12-13 21:57:17 -0800

    Completed PrintIfPrime

--- Puppet Coding Exercises/PrintIfPrime.rb
+++ Puppet Coding Exercises/PrintIfPrime.rb
@@ -1,8 +1,20 @@
-
 def isPrime( num1 )
-  if num1 % 2 == 0 && num2 > 2
-    return False
-  else
-    
+  if num1 % 2 == 0 && num1 > 2
+    return false
   end
+  (3..(Math.sqrt(num1).to_i+1)).step(2).all? {|i| num1 % i != 0}
+end
+
+def intSum( num2 )
+  sum = num2.to_s.chars.map(&:to_i).inject(0,:+)
+  return sum
+end
+
+x = 1
+while x < 100000
+  sum1  = intSum(x)
+  if isPrime(sum1)
+    puts "The sum of #{x}'s digits is #{sum1} and is prime."
+  end
+  x += 1
 end
